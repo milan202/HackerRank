3dd569cd0ebf534974fdbca5b17ee6ada7768edf
milan202 <milanperera1993@gmail.com>
2017-09-13 15:16:52 +0530

    Added Algorithms

--- ProjectEuler/ProjectEuler3.py
+++ ProjectEuler/ProjectEuler3.py
@@ -112,71 +112,100 @@ print "\n\nEnd time: ", str(datetime.datetime.now());
 print "Profiling finished for: ", sys.argv;
 print "#"*60"""
 
-import sys;
+# import sys;
 
-def nextprime(loop, current_index, prime_list):
-# Function to return the next prime to check for and also to populate the list array with numbers mapping to the current list items for the next 30 numbers.
-	if(current_index == 7):
-		loop = loop+1;
-		current_index = 0;
-	else:
-		current_index = current_index +1
-	return (loop * 30 + prime_list[current_index], loop, current_index, prime_list)
+# def nextprime(loop, current_index, prime_list):
+# # Function to return the next prime to check for and also to populate the list array with numbers mapping to the current list items for the next 30 numbers.
+# 	if(current_index == 7):
+# 		loop = loop+1;
+# 		current_index = 0;
+# 	else:
+# 		current_index = current_index +1
+# 	return (loop * 30 + prime_list[current_index], loop, current_index, prime_list)
 
-def checkDivisibility(number, val):
-# Strip all powers of subsequent primes from the number	till the number equal to the prime which divides it
-	i = 1;
-	factors = []
-	while(number%val == 0):
-		#print number, val**i;
-		factors.append(val**i);
-		number = number / val; # Reduce the number by factor of val
-		i = i + 1;
-	return (number, factors)
+# def checkDivisibility(number, val):
+# # Strip all powers of subsequent primes from the number	till the number equal to the prime which divides it
+# 	i = 1;
+# 	factors = []
+# 	while(number%val == 0):
+# 		#print number, val**i;
+# 		factors.append(val**i);
+# 		number = number / val; # Reduce the number by factor of val
+# 		i = i + 1;
+# 	return (number, factors)
 	
 	
-def updateFactors(factors, factor):
-	# Function to update the factors of the number with the factors found using checkDivisibility
-	if factor != [] :
-	# Append factor only if it is non empty
-		factors.append(factor);
-	return factors
+# def updateFactors(factors, factor):
+# 	# Function to update the factors of the number with the factors found using checkDivisibility
+# 	if factor != [] :
+# 	# Append factor only if it is non empty
+# 		factors.append(factor);
+# 	return factors
 	
 	
-def findFactors(number):
-	# Function to find all the factors of the number and to also print the largest factor of the number
-	val = 7;
-	loop = 0;
-	current_index = 1;
-	prime_list = [1,7,11,13,17,19,23,29]; # List to populate with all primes less than 30
+# def findFactors(number):
+# 	# Function to find all the factors of the number and to also print the largest factor of the number
+# 	val = 7;
+# 	loop = 0;
+# 	current_index = 1;
+# 	prime_list = [1,7,11,13,17,19,23,29]; # List to populate with all primes less than 30
 
-	factors = []; # Store all prime powers of a number
-	# Strip all powers of 2 from the number	
-	number, factor = checkDivisibility(number, 2);
-	factors = updateFactors(factors, factor);
+# 	factors = []; # Store all prime powers of a number
+# 	# Strip all powers of 2 from the number	
+# 	number, factor = checkDivisibility(number, 2);
+# 	factors = updateFactors(factors, factor);
 
-	# Strip all powers of 3 from the number
-	number, factor = checkDivisibility(number, 3);
-	factors = updateFactors(factors, factor);
+# 	# Strip all powers of 3 from the number
+# 	number, factor = checkDivisibility(number, 3);
+# 	factors = updateFactors(factors, factor);
 
-	# Strip all powers of 5 from the number
-	number, factor = checkDivisibility(number, 5);
-	factors = updateFactors(factors, factor);	
+# 	# Strip all powers of 5 from the number
+# 	number, factor = checkDivisibility(number, 5);
+# 	factors = updateFactors(factors, factor);	
 
-	# Strip all powers of subsequent primes from the number	till the number equal to the prime which divides it
-	while(number/val != 0):
-		number, factor = checkDivisibility(number, val);
-		factors = updateFactors(factors, factor);
-		#Get Next Prime to check for and simultaneously update loop, current_index and prime_list
-		val, loop, current_index, prime_list = nextprime(loop, current_index, prime_list)
-	#Append the last factor in the factor list
-	#factors.append([val])
-	#print "Largest Prime:", factors[-1][0]
-	return factors;
+# 	# Strip all powers of subsequent primes from the number	till the number equal to the prime which divides it
+# 	while(number/val != 0):
+# 		number, factor = checkDivisibility(number, val);
+# 		factors = updateFactors(factors, factor);
+# 		#Get Next Prime to check for and simultaneously update loop, current_index and prime_list
+# 		val, loop, current_index, prime_list = nextprime(loop, current_index, prime_list)
+# 	#Append the last factor in the factor list
+# 	#factors.append([val])
+# 	#print "Largest Prime:", factors[-1][0]
+# 	return factors;
 	
+# def findLargestPrimeFactor(number):
+# 	return findFactors(number)[-1][0];
+
+# #Run the code for number given below and print the largest factor along with the set of powers of prime factors
+# number = 18505779463480206643200
+# print findLargestPrimeFactor(number)
+
+import math
+
+def isPrime(x):
+   if (x is 2) or (x is 3):
+       return True
+   if (x is 1) or (x%2 is 0):
+       return False
+   for i in xrange(3,int(math.sqrt(x)+1),2):
+       if (x%i is 0):
+           return False
+   return True
+
+# number = 600851475143
+# largestFactor = 1
 def findLargestPrimeFactor(number):
-	return findFactors(number)[-1][0];
+    largestFactor = 1
+    for i in xrange(3,int(math.sqrt(number)),2):
+        if (number%i is 0):
+            if isPrime(i):
+                if (i > largestFactor):
+                    largestFactor = i
+            if (isPrime(number/i)):
+                largestFactor = number/i
+            
+    return largestFactor
 
-#Run the code for number given below and print the largest factor along with the set of powers of prime factors
-number = 18505779463480206643200
-print findLargestPrimeFactor(number)
+
+print findLargestPrimeFactor(6008514751430000000000000000000000000)
